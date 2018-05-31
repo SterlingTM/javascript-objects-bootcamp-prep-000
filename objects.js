b2f63ba8d398fdee0ec2158c2785f63c3e0a8f34
Sterling Moss <mtsilvertm@gmail.com>
2018-05-31 18:17:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,6 @@ var playlist = {artistName: 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, { artistName: [songTitle] });
+  playlist;
   return playlist;
 }
