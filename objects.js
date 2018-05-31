2b00334fa87db837816de638006dcdd2c1336be6
Sterling Moss <mtsilvertm@gmail.com>
2018-05-31 18:10:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,10 @@
-var playlist = { artistName: "songTitle"};
+var playlist = {"Phil Ochs": 'song'} 
 
-function updatePlaylist(playlist, artistName, songTitle) {
+function updatePlaylist(obj, key, value) {
   
-  return Object.assign({}, playlist, { [artistName]: songTitle});
+  return Object.assign({}, obj, { [key]: value})
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  
-  delete playlist.artistName;
-  return playlist;
+delete playlist.artistName
 }
