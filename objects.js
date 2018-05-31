8234700d1383826af8fc5ee5efe2a3a04a479349
Sterling Moss <mtsilvertm@gmail.com>
2018-05-31 16:22:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(obj, key, value) {
   
   return Object.assign({}, obj, { [key]: value})
 }
+
+function removeFromPlaylist(obj, key) {
+  delete obj.key;
+  return playlist
+}
